60f3e14666e67957e70c0ad5fc3600b4a0b88845
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-05 21:00:11 +0530

    Update FctoryMethodPatternTest.java

--- WEEK1 Design Pattern and Principles/Exercise2/FctoryMethodPatternTest.java
+++ WEEK1 Design Pattern and Principles/Exercise2/FctoryMethodPatternTest.java
@@ -1,1 +1,21 @@
+public class FactoryMethodPatternTest {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+        wordDoc.save();
+        wordDoc.close();
 
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+        pdfDoc.save();
+        pdfDoc.close();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+        excelDoc.save();
+        excelDoc.close();
+    }
+}
