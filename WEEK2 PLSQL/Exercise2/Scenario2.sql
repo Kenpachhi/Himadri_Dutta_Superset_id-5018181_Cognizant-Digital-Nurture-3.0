d4047e249a57c2c83f1f003ef4f7bb0a6cc04e87
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-05 23:57:46 +0530

    Update Scenario2.sql

--- WEEK2 PLSQL/Exercise2/Scenario2.sql
+++ WEEK2 PLSQL/Exercise2/Scenario2.sql
@@ -1,1 +1,31 @@
+CREATE OR REPLACE PROCEDURE UpdateSalary(
+    p_employee_id Employees.EmployeeID%TYPE,
+    p_percentage NUMBER
+) AS
+    v_employee_salary Employees.Salary%TYPE;
 
+BEGIN
+    -- Check if employee exists
+    BEGIN
+        SELECT Salary INTO v_employee_salary
+        FROM Employees
+        WHERE EmployeeID = p_employee_id;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Employee ID does not exist');
+    END;
+
+    -- Update salary
+    UPDATE Employees
+    SET Salary = Salary * (1 + p_percentage / 100)
+    WHERE EmployeeID = p_employee_id;
+
+    COMMIT;
+
+    DBMS_OUTPUT.PUT_LINE('Salary of employee '| p_employee_id || 'pdated successfully');
+
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE_APPLICATION_ERROR(-20002, 'Error updating salary: '| SQLERRM);
+END;
