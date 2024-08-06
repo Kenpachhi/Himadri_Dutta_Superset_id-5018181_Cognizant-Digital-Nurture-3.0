f0d03ea2ca19160fbc396720543aa0abd7cdbea0
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-06 12:42:20 +0530

    Update Scenario2.sql

--- WEEK2 PLSQL/Exercise6/Scenario2.sql
+++ WEEK2 PLSQL/Exercise6/Scenario2.sql
@@ -1,36 +1,35 @@
 DECLARE
-    -- Declare variables
+    
     v_account_id NUMBER;
     v_balance NUMBER;
     v_last_modified DATE;
-    v_annual_fee NUMBER := 50;  -- Annual maintenance fee
+    v_annual_fee NUMBER := 50;  
 
-    -- Declare cursor
+    
     CURSOR all_accounts IS
         SELECT AccountID, Balance, LastModified
         FROM Accounts;
 
-    -- Open cursor
+  
     BEGIN
         OPEN all_accounts;
 
-        -- Fetch and process each row
         LOOP
             FETCH all_accounts INTO v_account_id, v_balance, v_last_modified;
 
             EXIT WHEN all_accounts%NOTFOUND;
 
-            -- Deduct annual maintenance fee from balance
+            
             v_balance := v_balance - v_annual_fee;
 
-            -- Update account balance and last modified date
+            
             UPDATE Accounts
             SET Balance = v_balance,
                 LastModified = SYSTIMESTAMP
             WHERE AccountID = v_account_id;
         END LOOP;
 
-        -- Close cursor
+       
         CLOSE all_accounts;
     END;
 /
