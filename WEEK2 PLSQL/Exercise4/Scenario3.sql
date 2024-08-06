4389ed16772abea0433f004919a9d6a83fde13bf
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-06 12:25:24 +0530

    Update Scenario3.sql

--- WEEK2 PLSQL/Exercise4/Scenario3.sql
+++ WEEK2 PLSQL/Exercise4/Scenario3.sql
@@ -1,1 +1,19 @@
-
+CREATE OR REPLACE FUNCTION HasSufficientBalance(
+    p_AccountID NUMBER,
+    p_Amount NUMBER
+)
+RETURN BOOLEAN
+AS
+    v_Balance NUMBER;
+BEGIN
+    SELECT Balance INTO v_Balance
+    FROM Accounts
+    WHERE AccountID = p_AccountID;
+    
+    IF v_Balance >= p_Amount THEN
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+END;
+/
