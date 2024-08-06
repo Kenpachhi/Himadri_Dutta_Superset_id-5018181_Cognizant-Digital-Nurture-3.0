968c4b36461f7adbfab5198d0e8ba6acdc8b7c6b
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-06 12:35:02 +0530

    Update Scenario3.sql

--- WEEK2 PLSQL/Exercise5/Scenario3.sql
+++ WEEK2 PLSQL/Exercise5/Scenario3.sql
@@ -1,1 +1,22 @@
+CREATE OR REPLACE TRIGGER CheckTransactionRules
+BEFORE INSERT ON Transactions
+FOR EACH ROW
+DECLARE
+    v_account_balance NUMBER;
+BEGIN
+    SELECT Balance
+    INTO v_account_balance
+    FROM Accounts
+    WHERE AccountID = :NEW.AccountID;
 
+    IF :NEW.TransactionType = 'WITHDRAWAL' THEN
+        IF v_account_balance < :NEW.Amount THEN
+            RAISE_APPLICATION_ERROR(-20001, 'Withdrawal amount exceeds account balance');
+        END IF;
+    ELSIF :NEW.TransactionType = 'DEPOSIT' THEN
+        IF :NEW.Amount <= 0 THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Deposit amount must be positive');
+        END IF;
+    END IF;
+END;
+/
