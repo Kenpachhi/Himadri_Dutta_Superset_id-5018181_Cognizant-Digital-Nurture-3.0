fe0200a43d6676b773d46b22a23274af251fcd52
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-06 12:11:17 +0530

    Update Scenario2.sql

--- WEEK2 PLSQL/Exercise3/Scenario2.sql
+++ WEEK2 PLSQL/Exercise3/Scenario2.sql
@@ -1,1 +1,11 @@
-
+CREATE OR REPLACE PROCEDURE UpdateEmployeeBonus(
+    p_Department VARCHAR2,
+    p_BonusPercentage NUMBER
+)
+AS
+BEGIN
+    UPDATE Employees
+    SET Salary = Salary + (Salary * p_BonusPercentage / 100)
+    WHERE Department = p_Department;
+END;
+/
