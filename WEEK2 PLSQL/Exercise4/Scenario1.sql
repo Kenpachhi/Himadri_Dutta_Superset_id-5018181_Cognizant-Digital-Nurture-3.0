6f55a21629eaadf958876d80079c7f5977941907
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-06 12:20:18 +0530

    Update Scenario1.sql

--- WEEK2 PLSQL/Exercise4/Scenario1.sql
+++ WEEK2 PLSQL/Exercise4/Scenario1.sql
@@ -1,1 +1,11 @@
-
+CREATE OR REPLACE FUNCTION CalculateAge(
+    p_DOB DATE
+)
+RETURN NUMBER
+AS
+    v_Age NUMBER;
+BEGIN
+    v_Age := TRUNC(MONTHS_BETWEEN(SYSDATE, p_DOB) / 12);
+    RETURN v_Age;
+END;
+/
