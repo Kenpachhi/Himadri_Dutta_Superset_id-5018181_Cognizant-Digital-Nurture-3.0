48f89bcb1d5fbeb5045ff8d332fd14249281d1d9
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-06 12:22:53 +0530

    Update Scenario2.sql

--- WEEK2 PLSQL/Exercise4/Scenario2.sql
+++ WEEK2 PLSQL/Exercise4/Scenario2.sql
@@ -1,1 +1,19 @@
-
+CREATE OR REPLACE FUNCTION CalculateMonthlyInstallment(
+    p_LoanAmount NUMBER,
+    p_InterestRate NUMBER,
+    p_LoanDurationYears NUMBER
+)
+RETURN NUMBER
+AS
+    v_MonthlyInterestRate NUMBER;
+    v_LoanDurationMonths NUMBER;
+    v_MonthlyInstallment NUMBER;
+BEGIN
+    v_MonthlyInterestRate := p_InterestRate / 100 / 12;
+    v_LoanDurationMonths := p_LoanDurationYears * 12;
+    
+    v_MonthlyInstallment := p_LoanAmount * v_MonthlyInterestRate * POWER(1 + v_MonthlyInterestRate, v_LoanDurationMonths) / (POWER(1 + v_MonthlyInterestRate, v_LoanDurationMonths) - 1);
+    
+    RETURN ROUND(v_MonthlyInstallment, 2);
+END;
+/
