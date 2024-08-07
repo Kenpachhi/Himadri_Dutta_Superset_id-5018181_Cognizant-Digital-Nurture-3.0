3106d923d5e20fb202365e31bddd02a38b44a400
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-07 12:21:12 +0530

    Add files via upload

---
+++ WEEK2 SpringBoot_Maven/Library Management exercise(1-5)/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.library.repository;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BookRepository {
+
+    public List<String> getBooks() {
+        return Arrays.asList("Book 1", "Book 2", "Book 3");
+    }
+}
