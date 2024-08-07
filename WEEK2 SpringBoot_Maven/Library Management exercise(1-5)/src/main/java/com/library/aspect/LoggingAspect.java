5a9afd1a9c80a8f88176ca74c8b5086041da1514
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-07 12:19:52 +0530

    Add files via upload

---
+++ WEEK2 SpringBoot_Maven/Library Management exercise(1-5)/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,20 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object proceed = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+        System.out.println(joinPoint.getSignature() + " executed in " + (endTime - startTime) + "ms");
+        return proceed;
+    }
+}
