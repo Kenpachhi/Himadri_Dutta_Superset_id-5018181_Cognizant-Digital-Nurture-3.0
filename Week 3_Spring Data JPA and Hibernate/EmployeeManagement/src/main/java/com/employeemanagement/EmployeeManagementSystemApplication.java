4d2acbd8ca876b2bf1e096a1ed148badd6c9ce93
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-21 15:04:21 +0530

    Add files via upload

---
+++ Week 3_Spring Data JPA and Hibernate/EmployeeManagement/src/main/java/com/employeemanagement/EmployeeManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.employeemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
+public class EmployeeManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementSystemApplication.class, args);
+    }
+}
