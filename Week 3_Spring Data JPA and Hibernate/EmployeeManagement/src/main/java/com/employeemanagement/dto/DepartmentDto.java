fe83957d91f5025fdcaa479e762192a25e809eb1
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-21 14:31:39 +0530

    Add files via upload

---
+++ Week 3_Spring Data JPA and Hibernate/EmployeeManagement/src/main/java/com/employeemanagement/dto/DepartmentDto.java
@@ -0,0 +1,19 @@
+package com.employeemanagementsystem.dto;
+
+public class DepartmentDto {
+    private String name;
+
+    public DepartmentDto(String name) {
+        this.name = name;
+    }
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+    
+}
