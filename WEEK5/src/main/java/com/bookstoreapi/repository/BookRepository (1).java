728b30b82ab81e94a0b380a377f4286457743d73
Himadri Dutta <112925779+Kenpachhi@users.noreply.github.com>
2024-08-28 16:24:37 +0530

    Add files via upload

---
+++ WEEK5/src/main/java/com/bookstoreapi/repository/BookRepository (1).java
@@ -0,0 +1,9 @@
+package com.bookstoreapi.repository;
+
+import com.bookstoreapi.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
